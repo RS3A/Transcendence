b662d1ebf0d41c8263082c4de9cb17c0529a3a49
Leticia S <lesampietro@gmail.com>
2026-02-09 23:53:58 -0300

    fix: navbar itens fixed

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -1,14 +1,18 @@
 import './Header.css'
 
 function Header() {
-  return (
-    <header className="header">
-      Quem somos
-      Seja Mentor
-      Seja Mentorado
-      Logar
-    </header>
-  )
+	return (
+		<header className="header">
+			<nav>
+				<ul>
+					<li><a href="#quem-somos">Quem somos</a></li>
+					<li><a href="#seja-mentor">Seja Mentor</a></li>
+					<li><a href="#seja-mentorado">Seja Mentorado</a></li>
+					<li><button>Logar</button></li>
+				</ul>
+			</nav>
+		</header>
+	)
 }
 
 export default Header
