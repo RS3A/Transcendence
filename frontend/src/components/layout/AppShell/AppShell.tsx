dbcb881d3b208790a70afb49aa7d4be4df8ad308
Giovanna Gardinali <78451238+GioGardinali@users.noreply.github.com>
2026-03-17 09:28:31 -0300

    Revert "71 connection mongodb skills"

--- frontend/src/components/layout/AppShell/AppShell.tsx
+++ frontend/src/components/layout/AppShell/AppShell.tsx
@@ -1,23 +1,25 @@
-import { Outlet } from 'react-router-dom'
 import './AppShell.css'
 
-function AppShell({ sidebar, header, footer }) {
-  const hasAuth = !sidebar
+type AppShellProps = {
+  sidebar: React.ReactNode
+  header: React.ReactNode
+  footer: React.ReactNode
+  children: React.ReactNode
+}
 
+function AppShell({ sidebar, header, footer, children }: AppShellProps) {
   return (
-    <div className={`app-shell ${hasAuth ? 'auth' : ''}`}>
-      {sidebar && (
-        <aside className="sidebar">
-          {sidebar}
-        </aside>
-      )}
+    <div className="app-shell">
+      <aside className="sidebar">
+        {sidebar}
+      </aside>
 
       <header className="header">
         {header}
       </header>
 
       <main>
-        <Outlet />
+        {children}
       </main>
 
       <footer className="footer">
