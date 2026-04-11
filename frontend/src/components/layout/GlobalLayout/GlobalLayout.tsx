f514a8f89ddbecbbd5c6f3e76c6465371827c538
Leticia Sampietro <lesampietro@gmail.com>
2026-04-11 02:32:42 -0300

    fix consolidated css 2

--- frontend/src/components/layout/GlobalLayout/GlobalLayout.tsx
+++ frontend/src/components/layout/GlobalLayout/GlobalLayout.tsx
@@ -4,7 +4,7 @@ import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import { ChatWindow } from '../../chat/ChatWindow/ChatWindow'
 import AppShell from '../AppShell/AppShell'
-import '../AppShell/AppShell.css'
+import styles from '../AppShell/AppShell.module.css'
 import Chatbar from '../../chat/Chatbar/Chatbar'
 
 function GlobalLayout() {
